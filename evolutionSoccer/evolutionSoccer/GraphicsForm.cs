52da13651f3700a7ec2cb0c01fb49fb0987f5eed
Pavel Kravtsov <pavelkr.msc@gmail.com>
2016-03-04 04:26:26 +0300

    processing GraphicsForm

---
+++ evolutionSoccer/evolutionSoccer/GraphicsForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace evolutionSoccer
+{
+    public partial class GraphicsForm : Form
+    {
+
+        public GraphicsForm()
+        {
+            Console.WriteLine("Opening a form for graphs");
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            DrawCross();
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            DrawCross();
+        }
+
+        private void DrawCross()
+        {
+            System.Drawing.Graphics graphics = this.CreateGraphics();
+            graphics.Clear(Color.White);
+            graphics.DrawLine(System.Drawing.Pens.Black, new Point(this.Width / 20, 0), new Point(this.Width / 20, this.Height / 2));
+            graphics.DrawLine(System.Drawing.Pens.Black, new Point(0, this.Height / 2 - this.Width / 20), new Point(this.Width, this.Height / 2 - this.Width / 20));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DrawCross();
+        }
+
+    }
+}
--- evolutionSoccer/evolutionSoccer/Program.cs
+++ evolutionSoccer/evolutionSoccer/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
+using System.Windows.Forms;
 
 namespace evolutionSoccer
 {
     class Program
     {
+        [STAThread]
         static void Main()
         {
-            Season series1 = new Season("Russia", "Brasil", 500);
+            Season series1 = new Season("Russia", "Brasil", 200);
             series1.printTeams();
             series1.simulateSeason();
             series1.printTeams();
+            Application.EnableVisualStyles();
+            Application.Run(new GraphicsForm());   
             Console.ReadKey();
 
             //for (int i = 0; i < 35; i++) // Testing scores
